e25ead1ac308fafc79331faa7e7cec3bde05c642
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-16 11:12:41 +0100

    Improve ex02

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -1,9 +1,10 @@
-#ifndef BASE_HPP
-#define BASE_HPP
+#pragma once
 
 class Base {
 public:
     virtual ~Base();
 };
 
-#endif
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
