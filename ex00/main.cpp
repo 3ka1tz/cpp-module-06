28fc9387d04a953d624db0fbb48e9dff1e5705be
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-18 12:00:33 +0100

    Perfect main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,11 +1,14 @@
 #include "ScalarConverter.hpp"
 
+#include <cstdlib>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <literal>" << std::endl;
-        return 1;
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <literal>\n";
+        return EXIT_FAILURE;
     }
 
     ScalarConverter::convert(argv[1]);
