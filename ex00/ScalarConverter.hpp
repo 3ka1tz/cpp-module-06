9c32d8803b57c3f463e8e1153c2fcce2c4f2933e
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-18 12:02:49 +0100

    Perfect ScalarConverter.hpp

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -2,13 +2,14 @@
 
 #include <string>
 
-class ScalarConverter {
-public:
-    static void convert(const std::string& literal);
+class ScalarConverter
+{
+    public:
+        static void convert(const std::string& literal);
 
-private:
-    ScalarConverter();
-    ScalarConverter(const ScalarConverter& other);
-    ScalarConverter& operator=(const ScalarConverter& other);
-    ~ScalarConverter();
+    private:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter& other);
+        ScalarConverter& operator=(const ScalarConverter& other);
+        ~ScalarConverter();
 };
