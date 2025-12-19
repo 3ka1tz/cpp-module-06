01448d1576761219664212c658d9f54240df9594
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-19 12:09:16 +0100

    Minor stylistic change in ex01/main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,13 +4,18 @@
 
 int main() {
     Data d;
+
     d.id = 42;
     d.name = "Marvin";
 
     uintptr_t serialized = Serializer::serialize(&d);
     Data* deserialized = Serializer::deserialize(serialized);
 
-    std::cout << "    original_ptr: " << &d << "\n"
-              << "  serialized_ptr: " << serialized << "\n"
-              << "deserialized_ptr: " << deserialized << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "    original_ptr: " << &d << std::endl;
+    std::cout << "  serialized_ptr: " << serialized << std::endl;
+    std::cout << "deserialized_ptr: " << deserialized << std::endl;
+
+    std::cout << std::endl;
 }
