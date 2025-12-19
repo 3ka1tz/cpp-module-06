c99a91c3beedceab353f3b3df9c964dbf9d99edb
3ka1tz <ekaitzlopez2025@gmail.com>
2025-12-19 12:26:45 +0100

    Minor stylistic change in ex00/main.cpp

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+    std::cout << std::endl;
+
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <literal>\n";
+        std::cerr << "Usage: " << argv[0] << " <literal>\n" << std::endl;
         return 1;
     }
 
     ScalarConverter::convert(argv[1]);
+
+    std::cout << std::endl;
 }
